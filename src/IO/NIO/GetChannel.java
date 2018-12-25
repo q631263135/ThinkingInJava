dcc258930004580cfb799bb4bf9cbd7611fe0283
q631263135 <631263135@qq.com>
2018-12-25 22:11:37 +0800

    2018-12-25 22:11:32

---
+++ src/IO/NIO/GetChannel.java
@@ -0,0 +1,36 @@
+package IO.NIO;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by ycz on 2018/12/25.
+ */
+public class GetChannel {
+    private static final int BSIZE = 1024;
+
+    public static void main(String[] args) throws Exception {
+        FileChannel fc = new FileOutputStream("data.txt").getChannel();
+        fc.write(ByteBuffer.wrap("Some text".getBytes()));
+        fc.close();
+
+        fc = new RandomAccessFile("data.txt", "rw").getChannel();
+        fc.position(fc.size());
+        fc.write(ByteBuffer.wrap("Some more".getBytes()));
+        fc.close();
+
+        fc = new FileInputStream("data.txt").getChannel();
+        ByteBuffer buff = ByteBuffer.allocate(BSIZE);
+        fc.read(buff);
+        buff.flip();
+        while (buff.hasRemaining()) {
+            byte b = buff.get();
+            System.out.println(Integer.toBinaryString(b) + " " + b + " " + (char)b);
+        }
+
+    }
+}
