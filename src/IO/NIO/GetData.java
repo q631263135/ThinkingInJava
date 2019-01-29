6f97c1fa5a273e6719ee99069a8ecd21d396fc76
q631263135 <631263135@qq.com>
2019-01-29 22:54:09 +0800

    ByteBuffer获取基本数据类型

---
+++ src/IO/NIO/GetData.java
@@ -0,0 +1,56 @@
+package IO.NIO;
+
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+
+/**
+ * Created by ycz on 2019/1/21.
+ */
+public class GetData {
+    private final int BSIZE = 1024;
+
+    @Test
+    public void printTheAllocationOfByteBuffer() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(BSIZE);
+        System.out.println(byteBuffer.limit());
+
+        int i = 0;
+        while (i++ < byteBuffer.limit())
+            if (byteBuffer.get() != 0)
+                System.out.println("nonzero");
+
+        System.out.println(i);
+    }
+
+    @Test
+    public void storeAndReadChar() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(BSIZE);
+        byteBuffer.wrap("hello".getBytes()); // 我认为这个byteBuffer底层的数组，就是hello.getBytes的值
+
+        System.out.println(byteBuffer.remaining());
+
+        byteBuffer.flip();
+        System.out.println(byteBuffer.remaining());
+
+        while (byteBuffer.remaining() > 0) {
+//            byteBuffer.flip(); // 使用这条语句，会报错（java.nio.BufferUnderflowException）；不使用，打印都是 0
+            System.out.println(byteBuffer.get()); // 这里难道不是打印hello的字节表示吗？
+        }
+    }
+
+    @Test
+    public void storeAndReadCharCorrect() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(BSIZE);
+        byteBuffer.wrap("hello".getBytes()); // 我认为这个byteBuffer底层的数组，就是hello.getBytes的值
+
+//        CharBuffer charBuffer = byteBuffer.asCharBuffer();
+//        charBuffer.put("hello");
+        byteBuffer.put("hello".getBytes());
+
+        while (byteBuffer.remaining() > 0) {
+            System.out.print(byteBuffer.get() + " "); // 这里难道不是打印hello的字节表示吗？
+        }
+    }
+}
